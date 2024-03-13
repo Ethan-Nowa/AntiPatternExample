57fc790d2c62beb0b65814a53fd199785b10a9aa
Ethan-Nowa <140451419+Ethan-Nowa@users.noreply.github.com>
2024-03-13 01:34:55 -0400

    Update CircleProcessor.java, processCircle() and comments

--- CircleProcessor.java
+++ CircleProcessor.java
@@ -1,18 +1,43 @@
 
+// Performs calculations on a given circle's parameters.
 public class CircleProcessor {
-	public double calculateArea(double radius) {
+	private double radius;
+	private double height;
+
+	public CircleProcessor(double radius, double height) {
+		this.radius = radius;
+		this.height = height;
+	}
+	
+	// Calls the four calculations and prints results.
+	public void processCircle() {
+		System.out.println("Processing circle...");
+        double area = calculateArea();
+        System.out.println("Area of the circle with radius " + radius + ": " + area);
+        
+        double circumference = calculateCircumference();
+        System.out.println("Circumference of the circle with radius " + radius + ": " + circumference);
+        
+        double volume = calculateVolume();
+        System.out.println("Volume of the cylinder with radius " + radius + " and height " + height + ": " + volume);
+        
+        double surfaceArea = calculateSurfaceArea();
+        System.out.println("Surface area of the cylinder with radius " + radius + " and height " + height + ": " + surfaceArea);
+	}
+	
+	public double calculateArea() {
 		return Math.PI * radius * radius;
 	}
 
-	public double calculateCircumference(double radius) {
+	public double calculateCircumference() {
 		return 2 * Math.PI * radius;
 	}
 
-	public double calculateVolume(double radius, double height) {
+	public double calculateVolume() {
 		return Math.PI * radius * radius * height;
 	}
 
-	public double calculateSurfaceArea(double radius, double height) {
+	public double calculateSurfaceArea() {
 		return 2 * Math.PI * radius * (radius + height);
 	}
 }
