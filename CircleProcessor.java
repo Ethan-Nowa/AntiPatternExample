cfd2b51677fa6e9e0be48b62953e8733582a8b56
Ethan-Nowa <140451419+Ethan-Nowa@users.noreply.github.com>
2024-03-13 01:36:34 -0400

    fix indentation

--- CircleProcessor.java
+++ CircleProcessor.java
@@ -12,17 +12,17 @@ public class CircleProcessor {
 	// Calls the four calculations and prints results.
 	public void processCircle() {
 		System.out.println("Processing circle...");
-        double area = calculateArea();
-        System.out.println("Area of the circle with radius " + radius + ": " + area);
+        	double area = calculateArea();
+        	System.out.println("Area of the circle with radius " + radius + ": " + area);
         
-        double circumference = calculateCircumference();
-        System.out.println("Circumference of the circle with radius " + radius + ": " + circumference);
+        	double circumference = calculateCircumference();
+       	 	System.out.println("Circumference of the circle with radius " + radius + ": " + circumference);
         
-        double volume = calculateVolume();
-        System.out.println("Volume of the cylinder with radius " + radius + " and height " + height + ": " + volume);
+        	double volume = calculateVolume();
+        	System.out.println("Volume of the cylinder with radius " + radius + " and height " + height + ": " + volume);
         
-        double surfaceArea = calculateSurfaceArea();
-        System.out.println("Surface area of the cylinder with radius " + radius + " and height " + height + ": " + surfaceArea);
+        	double surfaceArea = calculateSurfaceArea();
+        	System.out.println("Surface area of the cylinder with radius " + radius + " and height " + height + ": " + surfaceArea);
 	}
 	
 	public double calculateArea() {
