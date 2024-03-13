c81dea47370a138426f20b34c8f9611f46828032
Ethan-Nowa <140451419+Ethan-Nowa@users.noreply.github.com>
2024-03-13 01:10:28 -0400

    Update CircleProcessor.java, remove statics

--- CircleProcessor.java
+++ CircleProcessor.java
@@ -1,18 +1,18 @@
 
 public class CircleProcessor {
-	public static double calculateArea(double radius) {
+	public double calculateArea(double radius) {
 		return Math.PI * radius * radius;
 	}
 
-	public static double calculateCircumference(double radius) {
+	public double calculateCircumference(double radius) {
 		return 2 * Math.PI * radius;
 	}
 
-	public static double calculateVolume(double radius, double height) {
+	public double calculateVolume(double radius, double height) {
 		return Math.PI * radius * radius * height;
 	}
 
-	public static double calculateSurfaceArea(double radius, double height) {
+	public double calculateSurfaceArea(double radius, double height) {
 		return 2 * Math.PI * radius * (radius + height);
 	}
 }
