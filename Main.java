f8eaf2b8e67217f254dc73983ca01f0f96732d42
Ethan-Nowa <140451419+Ethan-Nowa@users.noreply.github.com>
2024-03-13 01:37:39 -0400

    Update Main.java, clean it up and comments

--- Main.java
+++ Main.java
@@ -2,32 +2,24 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
-        List<Integer> numbers = generateRandomNumbers();
-        DataProcessor dataProcessor = new DataProcessor(numbers);
-        dataProcessor.processNumbers();
-
-        CircleProcessor circleProcessor = new CircleProcessor();
-        double radius = 5.0;
-        double area = circleProcessor.calculateArea(radius);
-        System.out.println("Area of the circle with radius " + radius + ": " + area);
+		// Generates a list of numbers and performs calculations on it.
+		List<Integer> numbers = generateRandomNumbers();
+        	DataProcessor dataProcessor = new DataProcessor(numbers);
+        	dataProcessor.processNumbers();
+		
         
-        double circumference = circleProcessor.calculateCircumference(radius);
-        System.out.println("Circumference of the circle with radius " + radius + ": " + circumference);
-        
-        double height = 10.0;
-        double volume = circleProcessor.calculateVolume(radius, height);
-        System.out.println("Volume of the cylinder with radius " + radius + " and height " + height + ": " + volume);
-        
-        double surfaceArea = circleProcessor.calculateSurfaceArea(radius, height);
-        System.out.println("Surface area of the cylinder with radius " + radius + " and height " + height + ": " + surfaceArea);
-    }
+        	// Performs calculations on a given circle's parameters.
+		double radius = 5.0, height = 10.0;
+        	CircleProcessor circleProcessor = new CircleProcessor(radius, height);
+        	circleProcessor.processCircle();
+    	}
 	
 	private static List<Integer> generateRandomNumbers() {
-        Random random = new Random();
-        List<Integer> numbers = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            numbers.add(random.nextInt(100));
-        }
-        return numbers;
-    }
+        	Random random = new Random();
+        	List<Integer> numbers = new ArrayList<>();
+        	for (int i = 0; i < 10; i++) {
+            	numbers.add(random.nextInt(100));
+        	}
+        	return numbers;
+    	}
 }
