b316d322b575330b66bb36b1b4a78f872fa24b90
Ethan-Nowa <140451419+Ethan-Nowa@users.noreply.github.com>
2024-03-13 00:56:32 -0400

    Fix The Blob

--- DataProcessor.java
+++ DataProcessor.java
@@ -73,22 +73,4 @@ public class DataProcessor {
 		}
 		return true;
 	}
-	
-	
-    // Golden Hammer: Overuse of a single tool (method) that may not be suited for the problem
-    public static double calculateArea(double length, double width) {
-        return length * width;
-    }
-
-    public static double calculatePerimeter(double length, double width) {
-        return 2 * (length + width);
-    }
-    
-    public static double calculateVolume(double length, double width, double height) {
-        return length * width * height;
-    }
-    
-    public static double calculateSurfaceArea(double length, double width, double height) {
-        return 2 * ((length * width) + (length * height) + (width * height));
-    }
 }
