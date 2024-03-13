4958c89684271ae86edfc06d9c68f0d9ca3e09e9
Ethan-Nowa <140451419+Ethan-Nowa@users.noreply.github.com>
2024-03-13 01:08:28 -0400

    Update CircleProcessor.java to function properly

--- CircleProcessor.java
+++ CircleProcessor.java
@@ -1,19 +1,18 @@
 
 public class CircleProcessor {
-    // Golden Hammer: Overuse of a single tool (method) that may not be suited for the problem
-    public static double calculateArea(double length, double width) {
-        return length * width;
-    }
+	public static double calculateArea(double radius) {
+		return Math.PI * radius * radius;
+	}
 
-    public static double calculatePerimeter(double length, double width) {
-        return 2 * (length + width);
-    }
-    
-    public static double calculateVolume(double length, double width, double height) {
-        return length * width * height;
-    }
-    
-    public static double calculateSurfaceArea(double length, double width, double height) {
-        return 2 * ((length * width) + (length * height) + (width * height));
-    }
+	public static double calculateCircumference(double radius) {
+		return 2 * Math.PI * radius;
+	}
+
+	public static double calculateVolume(double radius, double height) {
+		return Math.PI * radius * radius * height;
+	}
+
+	public static double calculateSurfaceArea(double radius, double height) {
+		return 2 * Math.PI * radius * (radius + height);
+	}
 }
